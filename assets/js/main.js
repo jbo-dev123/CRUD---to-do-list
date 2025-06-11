8211844e7b7cd30d21488ccb5182824f5565a5b2
jbo-dev123 <johnnybragueto@hotmail.com>
2025-06-11 17:06:00 -0300

    implementar salvar e restaurar tarefas com localStorage + botão apagar

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,15 +19,59 @@ function limpaInput() {
     inputTarefa.focus();
 }
 
+function criaBotaoApagar(li) {
+    li.innerHTML += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerHTML = 'Apagar'
+    li.appendChild(botaoApagar)
+    botaoApagar.setAttribute('class', 'apagar')
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa.')
+}
+
+document.addEventListener('click', function apagarTarefa(e) {
+    const el = e.target;
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+        salvarTarefas()
+    }
+})
+
 function criaTarefa(textoInput) {
     const li = criaLi();
     li.innerText = textoInput;
     tarefas.appendChild(li);
     limpaInput()
-    
+    criaBotaoApagar(li)
+    salvarTarefas()
 }
 
 botaoTarefa.addEventListener('click', function () {
     if (!inputTarefa.value) return;
     criaTarefa(inputTarefa.value)
 });
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto)
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas) || [];
+
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+    }
+
+}
+
+adicionaTarefasSalvas()
